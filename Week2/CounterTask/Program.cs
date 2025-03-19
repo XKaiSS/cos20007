9a43df2c374ec732db0ffe129a4ce2c6213cc3fc
xikai sun <a328011209@gmail.com>
2025-03-19 16:11:42 +1100

    Week1Homework

---
+++ Week2/CounterTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+namespace CounterTask;
+class Program
+{
+    public static void PrintCounters(Counter[]counters){
+     foreach(Counter counter in counters){
+        System.Console.WriteLine("{0} is {1}" , counter.Name, counter.Ticks);
+     }
+    }
+
+
+    static void Main(string[] args)
+    {
+        Counter[] myCounter = new Counter[3];
+        myCounter[0] = new Counter("Counter1");
+        myCounter[1] = new Counter("Counter2");
+        myCounter[2] = myCounter[0];
+        
+        for(long i = myCounter[0].Ticks ; i < 9; i++){
+            myCounter[0].Increment();
+        }
+        for(long i = myCounter[1].Ticks ; i < 14; i++){
+            myCounter[1].Increment();
+        }
+
+        myCounter[0].ResetByDefault();
+        myCounter[1].Reset();
+        
+        for(long i = myCounter[1].Ticks ; i < 14; i++){
+            myCounter[1].IncrementBy5();
+        }
+        PrintCounters(myCounter); //still run without any bugs/crash
+    }
+}
